97861047d9eaec4c9dfe743e4a44c7ad759b9d78
Cecil Beeland <beelandc@gmail.com>
2018-10-07 19:10:10 -0500

    Added test files w/ initial simple unit tests that check for HTTP-200 response on our three urls

---
+++ artwork_image_processor/tests/tests_models.py
@@ -0,0 +1,14 @@
+# Once you’ve written tests, run them using the test command of your project’s manage.py utility: $ ./manage.py test
+# Test discovery is based on the unittest module’s built-in test discovery. 
+# By default, this will discover tests in any file named “test*.py” under the current working directory.
+
+from django.test import TestCase
+
+# Create your tests here.
+#    def test_whatever_list_view(self):
+#        w = self.create_whateve:qr()
+#        url = reverse("whatever.views.whatever")
+#        resp = self.client.get(url)
+#
+#        self.assertEqual(resp.status_code, 200)
+#        self.assertIn(w.title, resp.content)
