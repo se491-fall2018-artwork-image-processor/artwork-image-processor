c64d157dbe349a709795a7226fd5bd010d03914f
Cecil Beeland <beelandc@gmail.com>
2018-10-16 12:13:53 -0500

    Added a UUID to Image model; Updated model_form_upload form/view to display results of run_style_transfer() method; Added additional form and model unit tests;

--- artwork_image_processor/models.py
+++ artwork_image_processor/models.py
@@ -1,12 +1,14 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 
 # This model is used to access the uploaded image from the user
-# Currently goes to site > static > media > static
+# Currently goes to media > uploaded_images
 class Image(models.Model):
     description = models.CharField(max_length=255, blank=True)
     imageFile = models.ImageField(upload_to='image_uploads/')
+    image_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     
     # for string representation
--- artwork_image_processor/tests/tests_forms.py
+++ artwork_image_processor/tests/tests_forms.py
@@ -2,7 +2,13 @@
 # Test discovery is based on the unittest module’s built-in test discovery. 
 # By default, this will discover tests in any file named “test*.py” under the current working directory.
 
+import os
+from django.conf import settings
+
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
+from artwork_image_processor.forms import *
+from artwork_image_processor.models import Image
 
 # Create your tests here.
 #    def test_whatever_list_view(self):
@@ -13,4 +19,21 @@ from django.test import TestCase
 #        self.assertEqual(resp.status_code, 200)
 #        self.assertIn(w.title, resp.content)
 
-# TODO: test image upload
+# Test image upload
+class FormsTestCase(TestCase):
+    
+    def test_image_upload_form(self):
+        
+        # Load in test image as file object
+        img = SimpleUploadedFile('static/unit_testing/0100.png', 
+                content=open(os.path.join(settings.MEDIA_ROOT, 'static/unit_testing/0100.png'), 'rb').read(), 
+                content_type="image/png")
+
+        # Create test data for form
+        form_data = {'description': 'something2134'}
+        file_data = {'imageFile': img}
+        form = ImageForm(form_data, file_data)
+
+        # Assert form is valid
+        self.assertTrue(form.is_valid())
+       
