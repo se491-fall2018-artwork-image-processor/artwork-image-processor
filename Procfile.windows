8bf7d68aeb0bbabee1edeebca502cf197a11fe3d
Unknown <kamiyokamiru@gmail.com>
2018-10-31 02:03:10 -0500

    reset
    debug = true

--- Procfile.windows
+++ Procfile.windows
@@ -1,3 +1,2 @@
 release: python manage.py migrate --run-syncdb
-release: python manage.py collectstatic --noinput
 web: newrelic-admin run-program gunicorn artwork_image_processor_site.wsgi
--- artwork_image_processor_site/settings.py
+++ artwork_image_processor_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@&n+hghx4ktg!%fr(qd-ve@!n03s3d*_x0k=d47z4@yag%eops'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['art-processor.herokuapp.com']
 
